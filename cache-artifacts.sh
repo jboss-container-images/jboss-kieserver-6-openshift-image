26f88779dbbc6261d6a3973955f1a7b2aac5ad3b
David Ward <errantepiphany@users.noreply.github.com>
2019-05-31 13:45:13 -0400

    [KIECLOUD-208] update 6.4.x artifacts to 6.4.12.GA (#57)
    Signed-off-by: David Ward <dward@redhat.com>

--- cache-artifacts.sh
+++ cache-artifacts.sh
@@ -2,18 +2,17 @@
 
 # Artifacts that are not in brew:
 
-# jboss-bpmsuite-6.4.12.GA-deployable-eap6.x.zip        | http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-bpmsuite/BPMS-6.4.12.CR1/jboss-bpmsuite-6.4.12.GA-deployable-eap6.x.zip
-# jboss-brms-bpmsuite-6.4.12.GA-supplementary-tools.zip | http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-bpmsuite/BPMS-6.4.12.CR1/jboss-brms-bpmsuite-6.4.12.GA-supplementary-tools.zip
+# jboss-eap-6.4.9-patch.zip          | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=45371
+# jboss-eap-6.4.19-patch.zip         | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=56031
+# jboss-eap-6.4.22-patch.zip         | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=68441
+# rh-sso-7.2.2-eap6-adapter.zip      | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=58101
+# rh-sso-7.2.2-saml-eap6-adapter.zip | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=58061
 
-# jboss-eap-6.4.0.zip                                   | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=37393
-# jboss-eap-6.4.9-patch.zip                             | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=45371
-# jboss-eap-6.4.19-patch.zip                            | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=56031
-# jboss-eap-6.4.22-patch.zip                            | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=68441
+# Artifact that is in brew, but the md5 sum lookup is returning a bad (404) artifact url:
+# https://brewweb.engineering.redhat.com/brew/buildinfo?buildID=410568
 
-# javax.json-1.0.4.jar                                  | http://central.maven.org/maven2/org/glassfish/javax.json/1.0.4/javax.json-1.0.4.jar
-
-# rh-sso-7.2.2-eap6-adapter.zip                         | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=57851&product=core.service.rhsso
-# rh-sso-7.2.2-saml-eap6-adapter.zip                    | https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=57811&product=core.service.rhsso
+# javax.json-1.0.4.jar (200) | http://download.eng.bos.redhat.com/brewroot/vol/rhel-7/packages/org.glassfish-javax.json/1.0.4/1/maven/org/glassfish/javax.json/1.0.4/javax.json-1.0.4.jar
+# javax.json-1.0.4.jar (404) | http://download.devel.redhat.com/brewroot/packages/org.glassfish-javax.json/1.0.4/1/maven/org/glassfish/javax/json/1.0.4/javax.json-1.0.4.jar
 
 D="${1:-.}"
 for Z in \
